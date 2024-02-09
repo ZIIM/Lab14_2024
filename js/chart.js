48122c1554854646ada289492672cca38ccb9784
John Thomas Gause III <johntgause12@gmail.com>
2024-02-08 16:20:30 -0800

    js is good along with css, js needs to be revisited and css could be better imo

--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,45 @@ let canvasElem = document.getElementById('chart')
  *
  */
 function renderChart() {
+    let appState = new AppState();
+    appState.loadItems();
+    // obj for chart
+    let data = {
+        labels: appState.allProducts.map(product => product.name),
+        datasets: [
+            {
+              label: 'Times Clicked',
+              data: appState.allProducts.map(product => product.timesClicked),
+              backgroundColor: 'rgba(75, 192, 192, 0.2)',
+              borderColor: 'rgba(75, 192, 192, 1)',
+              borderWidth: 1
+            },
+            {
+              label: 'Times Shown',
+              data: appState.allProducts.map(product => product.timesShown),
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+            }
+        ]
+        };
+    let options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+        y: {
+            beginAtZero: true
+            }
+        }
+    };
+        
+    // chart.js with the configuration and the canvasElem
+    let ctx = canvasElem.getContext('2d');
+        new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+        });
 }
 
 renderChart();
